11053c4f6c2fc449cddfceff93bd8a0e529134d1
peter6055 <59694246+peter6055@users.noreply.github.com>
2026-06-14 23:38:05 +1000

    fix(editor): enlarge comment thread height

--- src/components/editor/comments/CommentBubble.tsx
+++ src/components/editor/comments/CommentBubble.tsx
@@ -33,7 +33,7 @@ type ThreadBubbleProps = {
 
 type CommentBubbleProps = ComposeBubbleProps | ThreadBubbleProps;
 
-const THREAD_MAX_HEIGHT = "440px";
+const THREAD_MAX_HEIGHT = "560px";
 
 export function CommentBubble(props: CommentBubbleProps) {
   const { kind, position, onClose } = props;
