62a9a3f62afae4e9d627176e4a52620c368913c0
Peter Liu (Shih-Ming Liu) <59694246+peter6055@users.noreply.github.com>
2026-06-20 23:49:57 +1000

    chore: add allowedDevOrigins for 100.104.12.35 (#85)

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  allowedDevOrigins: ["100.118.120.*"],
+  allowedDevOrigins: ["100.118.120.*", "100.104.12.35"],
   turbopack: {
     root: process.cwd(),
   },
