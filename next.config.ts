6c5d2703822bac38c35556d07874ccd72ed8c442
Peter Liu <peter6055@gmail.com>
2026-07-28 14:01:17 +0000

    remove: incorrect config and file

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
 import type { NextConfig } from "next";
 
-const DEV_ORIGINS_DEFAULTS = [
-  "devenv-ubuntu-a.tail1f8d1a.ts.net",
-  "127.0.0.1",
-  "localhost",
-  "0.0.0.0",
-];
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS
     ? process.env.ALLOWED_DEV_ORIGINS.split(",").map((s) => s.trim())
-    : DEV_ORIGINS_DEFAULTS,
+    : [],
   turbopack: {
     root: process.cwd(),
   },
